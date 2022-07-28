60fd2a13421c6076333801ae05618ca23970c809
halitov-r-k <halitov.r.k@gmail.com>
2022-07-28 16:26:17 +0500

    2022-07-28

--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,10 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Program {
+    static Logger LOGGER = Logger.getLogger(Program.class.getName());
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Начало программы");
+
+    }
 }
--- src/Test1.java
+++ src/Test1.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Test1 {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.IntStream;
+
+public class Test1 {
+    static Logger LOGGER = Logger.getLogger(Test1.class.getName());
+    public static void main(String[] args) {
+        LOGGER.log(Level.INFO, "Начало Test1.");
+        int sum = IntStream.iterate(1, n -> n+1)
+                .filter(n -> n % 5 == 0 && n % 2 != 0)
+                .limit(10)
+                .map(n -> n * n)
+                .sum();
+        LOGGER.log(Level.INFO, "Результат {0}", sum);
+    }
 }
